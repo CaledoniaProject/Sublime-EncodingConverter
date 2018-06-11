2aa0fa8e9e74cab27e452f6d0d6bdfa6cc7b1d59
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-06-11 21:35:19 +0800

    Let us release that way

---
+++ converter.py
@@ -0,0 +1,38 @@
+import subprocess
+import sublime
+import sublime_plugin
+
+class BaseCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        if self.view.is_scratch():
+            sublime.error_message('File is scratch')
+            return
+
+        try:
+            region = sublime.Region(0, self.view.size())
+            data   = self.convert()
+
+            self.view.replace(edit, region, data)
+        except Exception as e:
+            sublime.error_message('Exception: ' + str(e))
+            raise
+
+    def convert(self):
+        result = subprocess.check_output([
+            'iconv', '-f', self.from_e, '-t', 'utf8//ignore', self.view.file_name()
+        ])
+
+        return result.decode('utf8')
+
+class ReopenAsGbkCommand(BaseCommand):
+    def run(self, edit):
+        self.from_e = 'gbk'
+        return super(ReopenAsGbkCommand, self).run(edit)
+
+class ReopenAsBig5Command(BaseCommand):
+    def run(self, edit):
+        self.from_e = 'big5'
+        return super(ReopenAsBig5Command, self).run(edit)
+
+
